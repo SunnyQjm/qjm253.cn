a1d957aa8310f18eacd7aef6c1458b2c680a5919
SunnyQjm <qjm253@gmail.com>
2018-10-17 21:27:26 +0800

    调整为按需引入组件

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@ export default {
   }
   #particle-effect{
     position: absolute;
-    z-index: 1;
+    z-index: 0;
     width: 100%;
     height: 100%;
     overflow: hidden;
--- src/main.js
+++ src/main.js
@@ -3,14 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui'
+import {
+  Button, Input, Card
+} from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'animate.css'
 
 import VueParticles from 'vue-particles'
 
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+Vue.use(Button)
+Vue.use(Input)
+Vue.use(Card)
 Vue.use(VueParticles)
 
 /* eslint-disable no-new */
